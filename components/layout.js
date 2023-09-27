8a5378bfb5aa535cea846b831798a56096645fe2
Ezekiel Adetoro <adetorodev19@gmail.com>
2023-09-27 16:23:30 -0500

    Dynamic Routes

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import styles from "./layout.module.css";
 import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
 
-const name = "Ezekiel Adetoro";
+const name = "Hi 👋, I'm Ezekiel Adetoro";
 export const siteTitle = "JavaScript|TypeScript Developer";
 
 export default function Layout({ children, home }) {
@@ -30,7 +30,7 @@ export default function Layout({ children, home }) {
           <>
             <Image
               priority
-              src="/images/ezek.jpg"
+              src="/images/profile.jpg"
               className={utilStyles.borderCircle}
               height={144}
               width={144}
@@ -43,7 +43,7 @@ export default function Layout({ children, home }) {
             <Link href="/">
               <Image
                 priority
-                src="/images/ezek.jpg"
+                src="/images/profile.jpg"
                 className={utilStyles.borderCircle}
                 height={108}
                 width={108}
