ed3c2d6c79625e8b28270bb2eded8d7ad8c8a578
Ezekiel Adetoro <adetorodev19@gmail.com>
2023-09-28 05:54:41 -0500

    dynamic routes

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,16 +1,22 @@
+import Head from 'next/head'
 import Layout from "../../components/layout";
 import { getAllPostIds, getPostData } from "../../lib/posts";
+import Date from '../../components/date';
+import utilStyles from '../../styles/utils.module.css';
 
 export default function Post({ postData }) {
   return (
     <Layout>
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      {postData.date}
-      <br />
-      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      <Head>
+      <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
     </Layout>
   );
 }
@@ -33,3 +39,18 @@ export async function getStaticProps({ params }) {
     },
   };
 }
+
+
+// export async function getAllPostIds() {
+//   // Instead of the file system,
+//   // fetch post data from an external API endpoint
+//   const res = await fetch('..');
+//   const posts = await res.json();
+//   return posts.map((post) => {
+//     return {
+//       params: {
+//         id: post.id,
+//       },
+//     };
+//   });
+// }
